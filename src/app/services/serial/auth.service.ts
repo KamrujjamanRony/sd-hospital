dd414360c1167ae6211d6ce9c71ab712bbb0856f
KamrujjamanRony <kamrujjaman0228@gmail.com>
2024-07-06 11:08:51 +0600

    user logout every 24h implement but not execute

--- src/app/services/serial/auth.service.ts
+++ src/app/services/serial/auth.service.ts
@@ -26,3 +26,55 @@ export class AuthService {
     localStorage.removeItem(this.localStorageKey);
   }
 }
+
+
+
+
+// import { Injectable, OnInit } from '@angular/core';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class AuthService implements OnInit {
+
+//   private localStorageKey = 'user';
+//   private deleteUserInterval: any;
+
+//   constructor() { }
+
+//   ngOnInit() {
+//     this.startDeleteUserTimer();
+//   }
+
+//   setUser(user: any) {
+//     // Save user to local storage
+//     localStorage.setItem(this.localStorageKey, JSON.stringify(user));
+//     this.startDeleteUserTimer(); // Restart the timer when a new user is set
+//   }
+
+//   getUser() {
+//     // Retrieve user from local storage
+//     const storedUser = localStorage.getItem(this.localStorageKey);
+//     return storedUser ? JSON.parse(storedUser) : null;
+//   }
+
+//   deleteUser() {
+//     // Remove user from local storage
+//     localStorage.removeItem(this.localStorageKey);
+//   }
+
+//   private startDeleteUserTimer() {
+//     if (this.deleteUserInterval) {
+//       clearInterval(this.deleteUserInterval);
+//     }
+    
+//     // Set a timeout to execute deleteUser in 24 hours
+//     this.deleteUserInterval = setTimeout(() => {
+//       this.deleteUser();
+//       // Set an interval to execute deleteUser every 24 hours
+//       this.deleteUserInterval = setInterval(() => {
+//         this.deleteUser();
+//       }, 24 * 60 * 60 * 1000); // 24 hours in milliseconds
+//     }, 24 * 60 * 60 * 1000); // Initial 24 hours in milliseconds
+//   }
+// }
